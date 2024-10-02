3142be5e2578368e27575bdb2735a48879521a1b
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-01 17:53:18 -0700

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that prints the Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """Function that prints out the Pascal's Triangle"""
+    triangle = []
+    
+    for i in range(n):
+        # Create a row with 1 at the beginning
+        row = [1]
+        # If we're beyond the first row, calculate the values between the 1s
+        if i > 0:
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)  # End with 1
+        # Add the row to the triangle
+        triangle.append(row)
+    
+    return triangle
