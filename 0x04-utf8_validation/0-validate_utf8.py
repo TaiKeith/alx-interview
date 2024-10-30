bf7a5707f7e6fed74263865ecceefbd32f29e1ae
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-30 23:16:35 +0300

    Created a function that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,7 +2,8 @@
 """
 This module contains a function that determines if a given data set represents
 a valid UTF-8 encoding.
-The function returns True if data is a valid UTF-8 encoding, else returns False.
+The function returns True if data is a valid UTF-8 encoding, else
+returns False.
 """
 
 
@@ -22,7 +23,7 @@ def validUTF8(data):
     for byte in data:
         # Only look at the last 8 bits of each integer (1 byte)
         byte = byte & 0xFF
-        
+
         if num_bytes == 0:
             # Determine the number of bytes in the character
             if (byte & mask1) == 0:
@@ -31,7 +32,8 @@ def validUTF8(data):
                 num_bytes = 1  # 2-byte character
             elif (byte & (mask1 >> 2)) == (mask1 | (mask1 >> 1)):
                 num_bytes = 2  # 3-byte character
-            elif (byte & (mask1 >> 3)) == (mask1 | (mask1 >> 1) | (mask1 >> 2)):
+            elif (byte & (mask1 >> 3)) == (
+                    mask1 | (mask1 >> 1) | (mask1 >> 2)):
                 num_bytes = 3  # 4-byte character
             else:
                 return False
