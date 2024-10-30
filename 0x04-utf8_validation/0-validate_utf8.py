64ee772464e50d3cdbb85078e0ce86fb16571e9f
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-30 23:18:31 +0300

    Created a function that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,34 +13,20 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, else return False.
     """
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    n_bytes, mask1, mask2 = 0, 1 << 7, 1 << 6
 
-    # Masks to check the leading bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for byte in data:
-        # Only look at the last 8 bits of each integer (1 byte)
-        byte = byte & 0xFF
-
-        if num_bytes == 0:
-            # Determine the number of bytes in the character
-            if (byte & mask1) == 0:
-                continue  # 1-byte character
-            elif (byte & (mask1 >> 1)) == mask1:
-                num_bytes = 1  # 2-byte character
-            elif (byte & (mask1 >> 2)) == (mask1 | (mask1 >> 1)):
-                num_bytes = 2  # 3-byte character
-            elif (byte & (mask1 >> 3)) == (
-                    mask1 | (mask1 >> 1) | (mask1 >> 2)):
-                num_bytes = 3  # 4-byte character
-            else:
+    for num in data:
+        mask = 1 << 7
+        if n_bytes == 0:
+            while mask & num:
+                n_bytes += 1
+                mask = mask >> 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            # Check that the byte starts with 10xxxxxx
-            if (byte & mask1) != mask1 or (byte & mask2) == mask2:
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+        n_bytes -= 1
+    return n_bytes == 0
